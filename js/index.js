3af64f04089687f36e124142b7ddedcfd6aea76a
sandeep saini <s.saini516@mybvc.ca>
2019-12-18 00:46:32 -0700

    create and push the project with project func.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var y = document.getElementById("tabbleId");
+    if (y.style.visibility === "hidden") {
+        y.style.visibility = "visible";
+      } else {
+        y.style.visibility = "hidden";
+      }
+}
